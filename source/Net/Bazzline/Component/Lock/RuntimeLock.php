838e86140775e28882839b78b3bf1e3488fa5544
stevleibelt <stev@leibelt.de>
2013-07-01 12:37:44 +0200

    Added runtimeLock

---
+++ source/Net/Bazzline/Component/Lock/RuntimeLock.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-01
+ */
+
+namespace Net\Bazzline\Component\Lock;
+
+/**
+ * Class RuntimeLock
+ *
+ * @package Net\Bazzline\Component\Lock
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-01
+ */
+class RuntimeLock extends LockAbstract
+{
+    /**
+     * @var boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-01
+     */
+    private $isLocked;
+
+    /**
+     * {$inheritDoc}
+     */
+    public function isLocked()
+    {
+        return ((!is_null($this->isLocked)) && ($this->isLocked == true));
+    }
+
+
+
+    /**
+     * {$inheritDoc}
+     */
+    public function acquire()
+    {
+		if ($this->isLocked()) {
+			throw new RuntimeException(
+                'Can not acquire lock, lock already exists.'
+            );
+		}
+
+        $this->isLocked = true;
+    }
+
+
+
+    /**
+     * {$inheritDoc}
+     */
+    public function release()
+    {
+		if (!$this->isLocked()) {
+			throw new RuntimeException(
+                'Can not release lock, no lock exists.'
+            );
+		}
+
+        $this->isLocked = false;
+    }
+}
